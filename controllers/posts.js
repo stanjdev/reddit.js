4b84da64004eafd21b836338bce1a4766a1029cc
Stanley Jeong <stanley.jeong@shopify.com>
2022-04-10 14:15:51 -0400

    users can see subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -25,12 +25,23 @@ module.exports = (app) => {
   });
 
   // SHOW - LOOK UP THE POST
-  app.get('/posts/:id', (req, res) => {
-    Post.findById(req.params.id).lean()
-      .then((post) => res.render('posts-show', { post }))
-      .catch((err) => {
-        console.log(err.message);
-      });
+  app.get('/posts/:id', async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id).lean();
+      return res.render('posts-show', { post });
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+  // SUBREDDIT
+  app.get('/n/:subreddit', async (req, res) => {
+    try {
+      const posts = await Post.find({ subreddit: req.params.subreddit }).lean();
+      return res.render('posts-index', { posts });
+    } catch(err) {
+      console.log(err);
+    }
   });
 
 
--- models/post.js
+++ models/post.js
@@ -4,6 +4,7 @@ const postSchema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
   summary: { type: String, required: true },
+  subreddit: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = model('Post', postSchema);

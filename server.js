876ae3d7f184c6f63978a7009b36d4fe6d7aba0d
Stanley Jeong <stanley.jeong@shopify.com>
2022-04-11 11:52:33 -0400

    users can sign up

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const { engine } = require('express-handlebars');
+const cookieParser = require('cookie-parser');
 
 // Set db
 require('./data/reddit-db');
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.engine('handlebars', engine());
@@ -18,6 +21,9 @@ require('./controllers/posts')(app);
 
 // COMMENTS RESOURCE
 require('./controllers/comments')(app);
+
+// AUTH RESOURCE
+require('./controllers/auth')(app);
 
 app.listen(3000);
 

5b4321f5dd25cfdfcb421b576c43866cf0c55295
Stanley Jeong <stanley.jeong@shopify.com>
2022-04-11 12:05:23 -0400

    users can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,6 +22,39 @@ module.exports = (app) => {
       });
   });
 
+  // LOGIN Form
+  app.get('/login', (req, res) => res.render('login'));
+
+  // LOGIN POST
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    // Find this user name
+    User.findOne({ username }, 'username password')
+      .then((user) => {
+        if (!user) {
+          // User not found
+          return res.status(401).send({ message: 'Wrong Username or Password' });
+        }
+        // Check the password
+        user.comparePassword(password, (err, isMatch) => {
+          if (!isMatch) {
+            // Password does not match
+            return res.status(401).send({ message: 'Wrong Username or Password' });
+          }
+          // Create a token
+          const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+            expiresIn: '60 days',
+          });
+          // Set a cookie and redirect to root
+          res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+          return res.redirect('/');
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   // LOG OUT
   app.get('/logout', (req, res) => {
     res.clearCookie('nToken');

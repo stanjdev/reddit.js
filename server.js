cbcabb70df93789ea8459fe8b6a3573f89b4b480
Stanley Jeong <stanley.jeong@shopify.com>
2022-04-11 22:10:04 -0400

    mime types express node static for stylesheet

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ require('./data/reddit-db');
 const app = express();
 
 app.use(cookieParser());
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(checkAuth);
